5525332fb1863810e86d6659e7ecdf62f4bd318e
Eric Carlos Quesada <ericcquesada@hotmail.com>
2023-06-14 12:28:13 -0300

    feature: Cadastro de login com chamada da api open-source: https://randomuser.me/api/

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,11 +3,9 @@ import React, { useState } from 'react';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isRegistered, setIsRegistered] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [isRegistrationError, setIsRegistrationError] = useState(false);
-  const [isLoginError, setIsLoginError] = useState(false);
   const [greeting, setGreeting] = useState('');
+  const [isError, setIsError] = useState(false);
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -17,100 +15,91 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
-  const handleRegister = () => {
-    // Verifica se o campo de e-mail e senha estão vazios, nulos ou em branco
+  const handleRegister = async () => {
     if (!email.trim() || !password.trim()) {
-      setIsRegistrationError(true);
-      setIsLoginError(false);
+      setIsError(true);
+      setGreeting('');
       return;
     }
 
-    // Aqui você pode adicionar a lógica para fazer o cadastro com as informações fornecidas
-    // Pode ser uma chamada a uma API, armazenamento local ou qualquer outra abordagem que desejar
+    try {
+      const response = await fetch('https://randomuser.me/api/');
+      const data = await response.json();
+      const user = data.results[0];
 
-    // Exemplo: Vamos simular um cadastro bem-sucedido após 2 segundos
-    setTimeout(() => {
-      setIsRegistered(true);
-      setIsRegistrationError(false);
-      setIsLoginError(false);
-    }, 2000);
+      const { first, last } = user.name;
+      const username = `${first} ${last}`;
+
+      setIsLoggedIn(true);
+      setGreeting(`Olá, ${username} seja bem-vindo ao sistema da Viveo!`);
+      setIsError(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const handleLogin = () => {
-    // Verifica se o campo de e-mail e senha estão vazios, nulos ou em branco
     if (!email.trim() || !password.trim()) {
-      setIsLoggedIn(false);
-      setIsLoginError(true);
-      setIsRegistrationError(false);
+      setIsError(true);
+      setGreeting('');
       return;
     }
 
     setIsLoggedIn(true);
-    setIsLoginError(false);
-    setIsRegistrationError(false);
-    setGreeting(`Olá, ${email} seja bem-vindo ao sistema da Vivio!`);
+    setGreeting(`Olá, ${email} seja bem-vindo ao sistema da Viveo!`);
+    setIsError(false);
   };
 
   return (
     <div className="container mx-auto max-w-xs">
       <h1 className="text-2xl font-bold mb-4">Tela de Login</h1>
-      {isRegistered ? (
-        <div>
-          <p className="mb-4">Cadastro realizado com sucesso!</p>
-          <p>E-mail: {email}</p>
-          <p>Password: ****</p>
-        </div>
-      ) : (
-        <div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-2">
-              E-mail:
-            </label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={handleEmailChange}
-              className="border border-gray-400 rounded px-2 py-1 w-full"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block mb-2">
-              Password:
-            </label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={handlePasswordChange}
-              className="border border-gray-400 rounded px-2 py-1 w-full"
-            />
-          </div>
-          <button
-            onClick={handleRegister}
-            className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded mr-2"
-          >
-            Cadastrar
-          </button>
-          <button
-            onClick={handleLogin}
-            className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded"
-          >
-            Entrar
-          </button>
+      <div className="mb-4">
+        <label htmlFor="email" className="block mb-2">
+          E-mail:
+        </label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          className="border border-gray-400 rounded px-2 py-1 w-full"
+        />
+      </div>
+      <div className="mb-4">
+        <label htmlFor="password" className="block mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          className="border border-gray-400 rounded px-2 py-1 w-full"
+        />
+      </div>
+      <div className="flex gap-2 mb-4">
+        <button
+          onClick={handleRegister}
+          className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded"
+        >
+          Cadastrar
+        </button>
+        <button
+          onClick={handleLogin}
+          className="bg-blue-500 hover:bg-blue-700 text-white py-1 px-4 rounded"
+        >
+          Entrar
+        </button>
+      </div>
+
+      {isLoggedIn && !isError && (
+        <div className="mt-4">
+          <p className="text-green-500">{greeting}</p>
         </div>
       )}
-      {isLoggedIn && (
-        <div>
-          <p className="text-green-500 mt-4">Login realizado com sucesso!</p>
-          <p>{greeting}</p>
-        </div>
-      )}
-      {isRegistrationError && (
-        <p className="text-red-500 mt-4">Cadastro inválido. Preencha todos os campos.</p>
-      )}
-      {isLoginError && (
-        <p className="text-red-500 mt-4">Login inválido. Verifique suas credenciais.</p>
+
+      {isError && (
+        <p className="text-red-500 mt-4">Preencha todos os campos corretamente.</p>
       )}
     </div>
   );
